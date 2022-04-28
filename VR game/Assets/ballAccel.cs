503bef211b6639c8814a57d3837887f2b508ca2d
Pgiannako <43864555+Pgiannako@users.noreply.github.com>
2022-04-29 01:09:49 +0300

    Game version 1.0
    1) added placeholder table
    2) sphere has rigidbody physics
    3) controls work with accelerometer
    4) MVP works
    5) added dummy script for gyro movement

---
+++ VR game/Assets/ballAccel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballAccel : MonoBehaviour
+{
+    //public bool isFlat = true;
+    private Rigidbody rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 tilt = Input.acceleration;
+        //if (isFlat)
+        tilt = Quaternion.Euler(180, 0, 0) * tilt;
+
+        rigid.AddForce(tilt);
+    }
+}
