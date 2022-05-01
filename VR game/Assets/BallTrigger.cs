e4c8c7c7f218a86e299badee9641f67edde45a7b
Pgiannako <43864555+Pgiannako@users.noreply.github.com>
2022-05-01 19:31:55 +0300

    Game Version 1.1
    Win/Lose condition added, scripts for detection added

---
+++ VR game/Assets/BallTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallTrigger : MonoBehaviour
+{
+
+    public ResultConditions resultScript;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < -10.0f)
+        {
+            resultScript.LoseCondition();
+        }
+    }
+}
---
+++ VR game/Assets/ResultConditions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultConditions : MonoBehaviour
+{
+    private bool gameEnd;
+
+    public void WinCondition()
+    {
+        if (!gameEnd)
+        {
+            Debug.Log("win");
+            gameEnd = true;
+        }
+    }
+
+    public void LoseCondition()
+    {
+        if (!gameEnd)
+        {
+            Debug.Log("lose");
+            gameEnd = true;
+        }
+        
+    }
+}
