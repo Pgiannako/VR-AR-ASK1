0f5527a637503cdda8b9432a7ff6996cf997e8dd
PBegkas <cs03286@uoi.gr>
2022-05-01 19:16:43 +0300

    2 new scenes
    Added 2 new scenes and some scripts

---
+++ VR game/Assets/disableROT.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disableROT : MonoBehaviour
+{
+
+    private Vector3 currentEulerAngles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //currentEulerAngles = transform.localEulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentEulerAngles = transform.localEulerAngles;
+        currentEulerAngles[1] = 0;
+        transform.eulerAngles = currentEulerAngles;
+    }
+}
