5e243be4f891933dc740608661719d90add7dcee
Pgiannako <43864555+Pgiannako@users.noreply.github.com>
2022-05-02 21:57:44 +0300

    game version 1.6
    Tutorial added and complete, transition to next level needed

---
+++ VR game/Assets/tutorialScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorialScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (transform.position.y < -20.0f)
+        {
+            Debug.Log("next scene");
+        }
+    }
+}
