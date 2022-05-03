02a3ad275c3c431e00ce3006d82e395aeb1f0a3e
Pgiannako <43864555+Pgiannako@users.noreply.github.com>
2022-05-03 15:09:29 +0300

    game version 1.7.3
    added sound effects to billiard levels 1-3

---
+++ VR game/Assets/audio_collision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audio_collision : MonoBehaviour
+{
+
+    AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision sound)
+    {
+        if (sound.gameObject.tag == "collision")
+        {
+            audioSource.Play();
+        }
+
+    }
+}
