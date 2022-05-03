43eaa03e3e7043d286d7cb4f6d83a2f1321e3007
PBegkas <cs03286@uoi.gr>
2022-05-03 13:39:56 +0300

    1.7
    added placeholder golf course

---
+++ VR game/Assets/FailHazard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FailHazard : MonoBehaviour
+{
+
+    public ResultConditions resultScript;
+
+    void Start()
+    {
+        resultScript.Initialise();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(other.gameObject);
+            resultScript.LoseCondition();
+        }
+    }
+}
---
+++ VR game/Assets/SlowdownHazard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowdownHazard : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.attachedRigidbody.drag = 5;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.attachedRigidbody.drag = 0;
+        }
+    }
+
+}
