921be4049c215f6b2211bcde7bb71358cefe7be5
Pgiannako <43864555+Pgiannako@users.noreply.github.com>
2022-05-04 17:35:26 +0300

    game version 1.14.3
    - fixed end scene performance
    -changed backround music on billiard
    -fixed transition

---
+++ VR game/Assets/Scripts/ball_despawn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball_despawn : MonoBehaviour
+{
+    void Update()
+    {
+        if (transform.position.y < -30.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
